33f9303987dd2607056ad2c70567ec6e8100623d
Teodoro <biel.teodorob@gmail.com>
2024-02-24 14:02:52 -0300

    refactor: Apply clean code on desafio 08

--- desafios/08-comentarios-vs-documentacao.js
+++ desafios/08-comentarios-vs-documentacao.js
@@ -1,28 +1,21 @@
-// Essa função tem o objetivo de registrar um novo usuário no banco.
-async function register(data) {
-  // Desestrutura os dados recebidos.
+async function registerUserInDatabase(data) {
   const { email, name, avatar } = data
 
-  // Verifica se o avatar foi informado.
   if (!avatar) return { error: 'avatar is required' }
 
-  // Verifica se o nome foi informado.
   if(!name) return { error: 'name is required' }
 
-  // Essa função retorna o usuário a partir do email informado
-  const userMail = getUserByEmail(email)
+  const emailAlreadyExists = getUserByEmail(email)
 
-  // Verificação se o email informado já foi utilizado por algum usuário.
-  if (userMail) {
+  if (emailAlreadyExists) {
     return { error: 'email already used' }
   }
 
   // Essa função realiza a conversão das imagens para JPG a fim de evitar erros de incompatibilidade.
   // Mais informações na issue https://github.com/rocketseat-education/example-repository/issues/1
-  const avatar2 = convertImageToJPG(avatar)
+  const convertedAvatar = convertImageToJPG(avatar)
 
-  // Cria o usuário no banco de dados
-  const user = await createUser({ email, name, avatar: avatar2 })
+  const createUser = await createUser({ email, name, avatar: convertedAvatar })
 
-  return { user }
+  return { createUser }
 }
