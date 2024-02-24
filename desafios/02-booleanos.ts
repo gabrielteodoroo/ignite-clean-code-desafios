567aadb34d1ccadfb9557fdbb28f571f7588066d
Teodoro <biel.teodorob@gmail.com>
2024-02-24 12:25:12 -0300

    refactor: Apply clean code on desafio 02

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -6,25 +6,25 @@ const user = {
   hasTicket: true,
 }
 
-const necessaryHeight = 130
+const necessaryHeightToEnterToy = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const userHasTicket = user.hasTicket
 
-if (!ticket) {
+if (!userHasTicket) {
   throw new Error('O Diego não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const canEnterToy = user.height > necessaryHeightToEnterToy
 
-if (!enterToy) {
+if (!canEnterToy) {
   throw new Error('O Diego não pode entrar no brinquedo!')
 } 
 
